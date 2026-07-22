318d4428d12e000ec8e64d3ef070a003e7a1abd9
Márcio Júnior <marcio.mj70@gmail.com>
2026-07-22 09:58:48 -0300

    chore: remove mcp client

--- src/ai/agent.ts
+++ src/ai/agent.ts
@@ -2,7 +2,6 @@ import { env } from "@/config";
 import { MemorySaver } from "@langchain/langgraph";
 import { ChatOpenAI } from "@langchain/openai";
 import { createAgent, ReactAgent } from "langchain";
-import { mcpClient } from "./tools/mcp";
 import { searchWeb } from "./tools/search";
 
 const model = new ChatOpenAI({
@@ -20,13 +19,12 @@ let agent: ReactAgent | null = null;
 export async function getAgent() {
   if (agent) return agent;
 
-  const mcpTools = await mcpClient.getTools();
   const checkpointer = new MemorySaver();
 
   agent = createAgent({
     model,
     checkpointer,
-    tools: [searchWeb, ...mcpTools],
+    tools: [searchWeb],
     systemPrompt: SYSTEM_PROMPT,
   });
   return agent;
